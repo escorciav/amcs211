0ecba0d94ccb07fedc1f0173f3de0edbe9aa07e3
Victor Escorcia <escorciav@gmail.com>
2015-03-15 01:05:16 -0500

    Systematic way of solving 3d

--- hw2/hw2_3d.m
+++ hw2/hw2_3d.m
@@ -1,7 +1,6 @@
 % HW2: Linear Programming - P3d
 % Author: Victor Escorcia
 % Date: March 2015
-c = [0; -1; -1; 1; 0];
 A = [0, 2, 2, 2, -4;
      0, -2, 2, -2, 0;
      4, 0, 2, 0, 2];
@@ -9,8 +8,13 @@ b = [2; 2; 2];
 C = [eye(size(A,2)); -eye(size(A,2))];
 d = ones(size(C,1), 1);
 
+x = [0; 1; 1; -1; 0];
+J = find((d - C*x) == 0);
+c = -(A'*[1; 1; 1] + C(J, :)'*[1; 1; 1])
+
 [x_p, f_x] = my_linprog(c, C, d, A, b);
 disp('Optimum:')
-disp(f_x)
+f_x
 disp('Minimizer:')
-disp(x_p)
+x_p
+disp(['x_p == x: ', num2str(all(x == x_p))])
