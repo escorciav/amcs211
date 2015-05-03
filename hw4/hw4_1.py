fe3990d48a4eb7d91967a3f49263e0e0a124a2bd
Victor Escorcia <escorciav@gmail.com>
2015-05-04 00:16:51 +0300

    Add bfgs method for line-search opt in hw4

--- hw4/hw4_1.py
+++ hw4/hw4_1.py
@@ -1,4 +1,5 @@
-import numpy as np
+from numpy import array, dot, eye, ones, zeros
+from numpy.linalg import inv, norm
 
 def hw4_1():
     """
@@ -7,7 +8,7 @@ def hw4_1():
     2. Newton algorithm
     3. Cauchy point
     4. Conjugate Gradient
-    5.
+    5. Quasi-newton method
 
     to minimize the Rosenbrock function (2.22). First try the initial point
     x0 = (1.2, 1.2)T and then the more difficult starting point x0 = (1.2, 1)T
@@ -15,24 +16,28 @@ def hw4_1():
     alpha_max , rho, c = 1, 0.5, 0.5
     f = (rb_function, rb_gradient, rb_hessian)
 
-    x_0 = np.array([[1.2], [1.2]])
+    x_0 = array([[1.2], [1.2]])
     x_p, f_obj, alpha, iter =  backtracking_min(f, x_0, 'steepest descent',
                                                 alpha_max, rho, c)
     #plot_results(x_p, f_obj, alpha, iter, 'steepest descent', x_0)
     x_p, f_obj, alpha, iter =  backtracking_min(f, x_0, 'newton', alpha_max,
                                                 rho, c)
     #plot_results(x_p, f_obj, alpha, iter, 'newton', x_0)
+    f = (rb_function, rb_gradient,
+         [rb_hessian(x_0), x_0.copy(), [], False])
+    x_p, f_obj, alpha, iter =  backtracking_min(f, x_0, 'quasi-newton',
+                                                alpha_max, rho, c)
     return None
 
 def backtracking_min(f, x_0, method, alpha_max=1, rho=0.5, c=0.5, iter=0,
                      tol=1e-4, max_iter=1e6):
     # Minimize f using line search
-    f_obj = np.zeros((max_iter, 1))
-    alpha = alpha_max * np.ones((max_iter, 1))
+    f_obj = zeros((max_iter, 1))
+    alpha = alpha_max * ones((max_iter, 1))
     alpha[0] = 0
 
     x, f_obj[iter] = x_0.copy(), f[0](x_0)
-    while np.linalg.norm(f[1](x)) > tol and iter < max_iter:
+    while norm(f[1](x)) > tol and iter < max_iter:
         iter += 1
         p = step_dir(f, x, method)
         alpha[iter], f_obj[iter] = step_length(f, x, p, alpha[iter], rho, c)
@@ -43,12 +48,23 @@ def backtracking_min(f, x_0, method, alpha_max=1, rho=0.5, c=0.5, iter=0,
 def step_dir(f, x_k, method):
     # Return a unit direction of search
     if method == 'newton' or method == 'n':
-        p = - np.dot(np.linalg.inv(f[2](x_k)), f[1](x_k))
+        p = - dot(inv(f[2](x_k)), f[1](x_k))
     elif method == 'quasi-newton' or method == 'qn':
-        p = 0
+        if not f[2][3]:
+            f[2][0] = inv(f[2][0])
+            f[2][2], f[2][3] = f[1](x_k), True
+        else:
+            n = x_k.ndim
+            x_k_1, g_x_k_1, g_x_k = f[2][1], f[2][2], f[1](x_k)
+            s, y = x_k - x_k_1, g_x_k - g_x_k_1
+            rho = 1 / dot(y.T, s)
+            A, B = eye(n) - rho*dot(s, y.T), eye(n) - rho*dot(y, s.T)
+            f[2][0] = dot(A, dot(f[2][0], B)) + rho*dot(s, s.T)
+            f[2][1], f[2][2] = x_k.copy(), g_x_k
+        p = - dot(f[2][0], f[2][2])
     else:
         p = - f[1](x_k)
-    p = p / np.linalg.norm(p)
+    p = p / norm(p)
     return p
 
 def step_length(f, x_k, p_k, alpha_max=1, rho=0.5, c=0.5):
@@ -56,7 +72,7 @@ def step_length(f, x_k, p_k, alpha_max=1, rho=0.5, c=0.5):
     alpha = alpha_max
     f_x_k = f[0](x_k)
     while (f[0](x_k + alpha * p_k) > 
-           f_x_k + c * alpha * np.dot(f[1](x_k).T, p_k)):
+           f_x_k + c * alpha * dot(f[1](x_k).T, p_k)):
         alpha = rho * alpha
     return alpha, f_x_k
 
@@ -79,14 +95,14 @@ xlabel('Iters');
 # Function to minimize, its gradient and hessian
 
 def rb_function(x):
-    return np.array([(100*(x[1, 0] - x[0, 0]**2)**2 + (1 - x[0, 0])**2)])
+    return array([(100*(x[1, 0] - x[0, 0]**2)**2 + (1 - x[0, 0])**2)])
 
 def rb_gradient(x):
-    return np.array([[2*x[0, 0] - 400*x[0, 0] * (-x[0, 0]**2 + x[1, 0]) - 2],
-                     [200*(x[1, 0] - x[0, 0]**2)]])
+    return array([[2*x[0, 0] - 400*x[0, 0] * (-x[0, 0]**2 + x[1, 0]) - 2],
+                  [200*(x[1, 0] - x[0, 0]**2)]])
 
 def rb_hessian(x):
-    return np.array([[(2 + 1200*x[0, 0]**2 - 400*x[1, 0]), (-400*x[0, 0])],
-                     [(-400*x[0]), 200]])
+    return array([[(2 + 1200*x[0, 0]**2 - 400*x[1, 0]), (-400*x[0, 0])],
+                  [(-400*x[0]), 200]])
 
 hw4_1()
